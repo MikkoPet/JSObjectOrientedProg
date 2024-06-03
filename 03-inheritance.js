1f4c549cdb36623eea23e3212f8c82b2025cec86
ShMPt <cacahouettetbs@gmail.com>
2024-06-03 14:35:09 +0200

    feat: complete third exercice

---
+++ 03-inheritance.js
@@ -0,0 +1,42 @@
+class Vehicle {
+    _make = "Renault";
+    _model = "Clio";
+    _year = "2020";    
+
+    setMake(make) {
+        this._make = make;
+    }
+
+    getMake() {
+        return this._make;
+    }
+
+    setModel(model) {
+        this._model= model;
+    }
+
+    getModel() {
+        return this._make;
+    }
+
+    setYear(year) {
+        this._year = year;
+    }
+
+    getYear() {
+        return this._year;
+    }
+
+};
+
+class Car extends Vehicle {
+    _color = "Red"
+
+    getColor() {
+        return this._color;
+    }
+
+    setColor(color) {
+        this._color = color;
+    }
+};
