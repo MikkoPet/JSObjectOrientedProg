3df34791d688be08ba69b5df750f59a20ee38f0e
ShMPt <cacahouettetbs@gmail.com>
2024-06-03 14:07:47 +0200

    feat: complete first exercice

---
+++ 01-classes-and-objects.js
@@ -0,0 +1,17 @@
+"use strict";
+
+class Car {
+    constructor(make, model, year, color) {
+    this.make = make;
+    this.model = model;
+    this.year = year;
+    this.color = color;
+    }
+
+    getCarData() {
+        alert(make + " " + model + " " + year + " " + color);
+    }
+}
+
+let myCar = new Car("Renault", "Clio", "2020", "Red");
+myCar.getCarData;
